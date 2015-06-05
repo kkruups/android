afc6bd7c14d17d87333f7a4ea817d653abefbc00
kkruups <kkruups@gmail.com>
2015-06-06 04:48:15 +0900

    Update android plugin from 1.3.0-beta1 to 1.3.0-beta2 Wired all logic for toggle buttons and launchApp method to show appropriate toasts.

--- app/src/main/java/com/kdomain/kkruups/myappportfolio/MainActivity.java
+++ app/src/main/java/com/kdomain/kkruups/myappportfolio/MainActivity.java
@@ -2,16 +2,35 @@ package com.kdomain.kkruups.myappportfolio;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+import android.content.Context;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import static android.widget.Toast.*;
+import static com.kdomain.kkruups.myappportfolio.R.id.toggleButton1;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+   
+
+    private View tButton;
+    Context context=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
     @Override
@@ -35,4 +54,56 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void launchApp(View tButton){
+           this.tButton = tButton;
+
+
+        int tBtn = this.tButton.getId();
+        context = getApplicationContext();
+        Toast toast;
+        CharSequence text;
+        switch(tBtn){
+
+            case toggleButton1:
+                text  = "Application 1";
+                toast=Toast.makeText(context, text, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 500);
+                toast.show();
+                break;
+            case R.id.toggleButton2:
+                text= "Application 2";
+                toast=Toast.makeText(context, text, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 500);
+                toast.show();
+                break;
+            case R.id.toggleButton3:
+                text= "Application Library";
+                toast=Toast.makeText(context, text, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 500);
+                toast.show();
+                break;
+            case R.id.toggleButton4:
+                text= "Application 3";
+                toast=Toast.makeText(context, text, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 500);
+                toast.show();
+                break;
+            case R.id.toggleButton5:
+                text= "Application 4";
+                toast=Toast.makeText(context, text, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 500);
+                toast.show();
+                break;
+            default:
+                text="something is not right";
+                toast=Toast.makeText(context, text, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 500);
+                toast.show();
+                break;
+
+        }
+    }
+
+
 }
