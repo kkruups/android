37cb9dda88edbd80afa86de063eb9dba8727163f
kk <kkruups@gmail.com>
2015-06-05 15:41:54 +0900

    test change

--- app/src/main/java/com/kdomain/kkruups/myappportfolio/MainActivity.java
+++ app/src/main/java/com/kdomain/kkruups/myappportfolio/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+//test change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
